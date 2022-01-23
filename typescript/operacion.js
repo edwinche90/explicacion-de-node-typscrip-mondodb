e915ce755b4d90910feecafff28ea4690f828a7e
edwinche90 <78755895+edwinche90@users.noreply.github.com>
2022-01-22 19:08:07 -0600

    22 de enero 2022

---
+++ typescript/operacion.js
@@ -0,0 +1,5 @@
+function suma(a, b) {
+    return a + b;
+}
+var result = suma(10, 45);
+console.log("Resultado de la suma es: ".concat(result));
---
+++ typescript/operacion.ts
@@ -0,0 +1,6 @@
+function suma(a:number, b:number):number{
+    return a + b;
+}
+
+var result:number = suma(10,45);
+console.log(`Resultado de la suma es: ${result}`);
